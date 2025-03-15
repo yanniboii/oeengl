69150b2887c9aad343e44aedd07b2841fccbde44
Yannek <yannekzessin@gmail.com>
2025-03-15 14:51:40 +0900

    fix: changed the texture wrap so it doesn't repeat 1 pixel on the other side of the screen

--- src/Abstraction/LowAbstraction/TextureBuffer.cpp
+++ src/Abstraction/LowAbstraction/TextureBuffer.cpp
@@ -4,29 +4,31 @@
 TextureBuffer::TextureBuffer(GLenum target, GLsizei width, GLsizei height)
 	:target(target), width(width), height(height)
 {
-    glGenTextures(1, &m_RendererID);
-    glBindTexture(target, m_RendererID);
-    glTexImage2D(target, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-    glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glGenTextures(1, &m_RendererID);
+	glBindTexture(target, m_RendererID);
+	glTexImage2D(target, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 TextureBuffer::~TextureBuffer()
 {
-    glDeleteTextures(1, &m_RendererID);
+	glDeleteTextures(1, &m_RendererID);
 }
 
 void TextureBuffer::AttachToFrameBuffer(GLenum target)
 {
-    glFramebufferTexture2D(target, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_RendererID, 0);
+	glFramebufferTexture2D(target, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_RendererID, 0);
 }
 
 void TextureBuffer::Bind() const
 {
-    glBindTexture(target, m_RendererID);
+	glBindTexture(target, m_RendererID);
 }
 
 void TextureBuffer::Unbind() const
 {
-    glBindTexture(target, 0);
+	glBindTexture(target, 0);
 }
